7017457ecd31823bbc97517a73ec9302ab153437
Hemish Reddy <hemishreddy@Hemish-ka-MacBook-Air.local>
2026-05-14 13:06:57 +0530

    added ranking

--- co.cpp
+++ co.cpp
@@ -4,51 +4,78 @@ using namespace std;
 
 class Document{
     ll docID;
-    string Title;
-    string Content;
-    public:
-        Document(int id,string title,string content):docID(id) ,Title(title),Content(content){}
-        ll getID() const{return docID;}
-        const string getTitle() const{return Title;}
-        string getContent() const{return Content;}
+    string title;
+    string content;
+public:
+    Document(ll id,const string& Title,const string& Content): docID(id),title(Title),content(Content) {}
+    ll getID() const {
+        return docID;
+    }
+    const string& getTitle() const {
+        return title;
+    }
+    const string& getContent() const {
+        return content;
+    }
 };
-class Tokenizer{
-    public:
-        vector<string> tokenize(const string& text) const {
-            vector<string> tokens;
-            stringstream ss(text);
-            string word;
-            while (ss >> word) {
-                transform(word.begin(), word.end(),word.begin(),::tolower);
-                word.erase(remove_if(word.begin(),word.end(),::ispunct),word.end());
-                if(!word.empty()) tokens.push_back(word);
-            }
-            return tokens;
-        }
 
+class Tokenizer {
+public:
+    vector<string>tokenize(const string& text) const {
+        vector<string> tokens;
+        stringstream ss(text);
+        string word;
+        while (ss >> word) {
+            transform(word.begin(),word.end(),word.begin(),::tolower);
+            word.erase(remove_if(word.begin(),word.end(),::ispunct),word.end());
+            if (!word.empty()) {
+                tokens.push_back(word);
+            }
+        }
+        return tokens;
+    }
 };
 
 class InvertedIndex {
     unordered_map<string,unordered_map<ll,int>> index;
-    public:
-        void addDocument(ll docID,const vector<string>& tokens) {
-            for(const string& token : tokens)    index[token][docID]++;
-            
+public:
+    void addDocument(ll docID,const vector<string>& tokens) {
+        for (const string& token : tokens)
+            index[token][docID]++;
+    }
+    unordered_map<ll,int>searchWord(const string& word) const {
+        auto it = index.find(word);
+        if (it == index.end()) {
+            return {};
         }
-        vector<ll> searchWord(const string& word) {
-            vector<ll> result;
-            if(index.find(word) == index.end()) return result;
-            for(auto& entry : index[word]) {
-                result.push_back(entry.first);
-            }
-            return result;
+        return it->second;
+    }
+    void deleteDocument(ll docID) {
+        for (auto& wordEntry : index) {
+            wordEntry.second.erase(docID);
         }
-        void deleteDocument(ll docID) {
-            for(auto& wordEntry : index) {
-                wordEntry.second.erase(docID);
-            }
-        }
+    }
 };
 
-
-
+class RankingEngine {
+public:
+    vector<pair<ll,int>>
+    rankings(const unordered_map<ll,int>& docs) const {
+        priority_queue<pair<int,ll>> pq;
+        for (const auto& entry : docs) {
+            ll docID = entry.first;
+            int frequency = entry.second;
+            pq.push({frequency, docID});
+        }
+        vector<pair<ll,int>> rankedResults;
+        while (!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            rankedResults.push_back({
+                top.second,
+                top.first
+            });
+        }
+        return rankedResults;
+    }
+};
